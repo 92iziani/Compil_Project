b0479f556753dd3fa7e8fae73db253653264f1bf
Laury Thiebaux <laury.thiebaux@telecomnancy.eu>
2021-11-27 11:12:39 +0100

    graphVizVisitor premieres fonctions commentees

---
+++ src/GraphVizVisitor/GraphVizVisitor.java
@@ -0,0 +1,248 @@
+package GraphVizVisitor;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import ast.*;
+
+public class GraphVizVisitor implements AstVisitor<String> {
+
+    private int state;
+    private String nodeBuffer;
+    private String linkBuffer;
+
+    public GraphVizVisitor(){
+        this.state = 0;
+        this.nodeBuffer = "digraph \"ast\"{\n\n\tnodesep=1;\n\tranksep=1;\n\n";
+        this.linkBuffer = "\n";
+    }
+
+    public void dumpGraph(String filepath) throws IOException{
+            
+        FileOutputStream output = new FileOutputStream(filepath);
+
+        String buffer = this.nodeBuffer + this.linkBuffer;
+        byte[] strToBytes = buffer.getBytes();
+
+        output.write(strToBytes);
+
+        output.close();
+
+    }
+
+
+    private String nextState(){
+        int returnedState = this.state;
+        this.state++;
+        return "N"+ returnedState;
+    }
+
+    private void addTransition(String from,String dest){
+        this.linkBuffer += String.format("\t%s -> %s; \n", from,dest);
+
+    }
+
+    private void addNode(String node,String label){
+        this.nodeBuffer += String.format("\t%s [label=\"%s\", shape=\"box\"];\n", node,label);
+
+    }
+
+    public String visit(Program program) {
+        String nodeIdentifier = this.nextState();
+
+        String instructionsState =program.declarations.accept(this);
+
+        this.addNode(nodeIdentifier, "Program");
+        this.addTransition(nodeIdentifier, instructionsState);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(IfThen ifthen) {
+        String nodeIdentifier = this.nextState();
+
+        String conditionState = ifthen.condition.accept(this);
+        String thenBlockState = ifthen.thenBlock.accept(this);
+
+        this.addNode(nodeIdentifier, "IfThen");
+
+        this.addTransition(nodeIdentifier, conditionState);
+        this.addTransition(nodeIdentifier, thenBlockState);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(IfThenElse ifthenelse) {
+        String nodeIdentifier = this.nextState();
+
+        String conditionState = ifthenelse.condition.accept(this);
+        String thenBlockState = ifthenelse.thenBlock.accept(this);
+        String elseBlockState = ifthenelse.elseBlock.accept(this);
+
+        this.addNode(nodeIdentifier, "IfThenElse");
+
+        //ici il y a 3 branches reliees au noeud courant
+        this.addTransition(nodeIdentifier, conditionState);
+        this.addTransition(nodeIdentifier, thenBlockState);
+        this.addTransition(nodeIdentifier, elseBlockState);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(While whil) {
+        String nodeIdentifier = this.nextState();
+
+        this.addNode(nodeIdentifier, "While");
+
+        String cond = whil.condition.accept(this);
+        String ins = whil.instruction.accept(this);
+
+        this.addTransition(cond,ins);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(Return retur) {
+        String nodeIdentifier = this.nextState();
+
+        this.addNode(nodeIdentifier, "Return");
+
+        String value = retur.retour.accept(this);
+        
+        this.addTransition(nodeIdentifier,value);
+        return nodeIdentifier;
+    }
+
+    public String visit(Affectation affect) {
+        //noeud que l'on va retourner
+        String nodeIdentifier = this.nextState();
+
+        //ici partie de gauche ident
+        String identState = affect.ident.accept(this);
+
+        //ici partie de droite expression
+        String expressionState = affect.expr.accept(this);
+
+        this.addNode(nodeIdentifier, "="); //affectation
+        //ajout d'un lien entre le noeud courant et ident
+        this.addTransition(nodeIdentifier,identState);
+        //ajout d'un lien entre le noeud courant et expression
+        this.addTransition(nodeIdentifier,expressionState);
+
+        //on retourne le noeud courant
+        return nodeIdentifier;
+    }
+
+    public String visit(Ident ident) {
+        String nodeIdentifier = this.nextState();
+
+        this.addNode(nodeIdentifier, ident.name);
+
+        return nodeIdentifier;
+
+    }
+
+    public String visit(ListeDeclVars liste) {
+        String nodeIdentifier = this.nextState();
+
+        this.addNode(nodeIdentifier, "InstrList");
+
+        for (Ast ast:liste.instrList){
+
+            String astState = ast.accept(this);
+            //ajout d'un lien pour chaque fils
+            this.addTransition(nodeIdentifier, astState);
+
+        }
+
+        return nodeIdentifier;
+    }
+
+    public String visit(ListeInstruction liste) {
+        String nodeIdentifier = this.nextState();
+
+        this.addNode(nodeIdentifier, "InstrList");
+
+        for (Ast ast:liste.instrList){
+
+            String astState = ast.accept(this);
+            //ajout d'un lien pour chaque fils
+            this.addTransition(nodeIdentifier, astState);
+
+        }
+
+        return nodeIdentifier;
+    }
+
+    public String visit(Bloc bloc) {
+        String nodeIdentifier = this.nextState();
+
+        String dec = bloc.declarations.accept(this);
+        String instr = bloc.instructions.accept(this);
+
+        this.addTransition(nodeIdentifier,dec);
+        this.addTransition(nodeIdentifier,instr);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(Plus plus) {
+        String nodeIdentifier = this.nextState();
+
+        String leftState = plus.left.accept(this);
+        String rightState = plus.right.accept(this);
+
+        this.addNode(nodeIdentifier, "+");
+
+        this.addTransition(nodeIdentifier, leftState);
+        this.addTransition(nodeIdentifier, rightState);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(Minus minus) {
+        String nodeIdentifier = this.nextState();
+
+        String leftState = minus.left.accept(this);
+        String rightState = minus.right.accept(this);
+
+        this.addNode(nodeIdentifier, "-");
+
+        this.addTransition(nodeIdentifier, leftState);
+        this.addTransition(nodeIdentifier, rightState);
+
+        return nodeIdentifier;
+    }
+
+    public String visit(Divide divide) {
+
+        String nodeIdentifier = this.nextState();
+
+        String leftState = divide.left.accept(this);
+        String rightState = divide.right.accept(this);
+
+        //nom du nom que l'on souhaite afficher
+        this.addNode(nodeIdentifier, "/");
+
+        this.addTransition(nodeIdentifier, leftState);
+        this.addTransition(nodeIdentifier, rightState);
+
+        return nodeIdentifier;
+
+    }
+
+    public String visit(Mult mult) {
+        String nodeIdentifier = this.nextState();
+
+        String leftState = mult.left.accept(this);
+        String rightState = mult.right.accept(this);
+
+        this.addNode(nodeIdentifier, "*");
+
+        this.addTransition(nodeIdentifier, leftState);
+        this.addTransition(nodeIdentifier, rightState);
+
+        return nodeIdentifier;
+    }
+
+}
