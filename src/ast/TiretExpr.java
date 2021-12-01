3c20c1715bc074fd754bcecd82340fd668112bdd
ZIANI Ahmed <ahmed.ziani@telecomnancy.eu>
2021-12-01 13:22:11 +0000

    Upload New File

---
+++ src/ast/TiretExpr.java
@@ -0,0 +1,15 @@
+package ast;
+
+public class TiretExpr implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast expr;
+
+    public TiretExpr(Ast expr){
+        this.expr = expr;
+    }
+}
+
