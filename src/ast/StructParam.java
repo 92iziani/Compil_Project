a2051feebaff951cd52e8629fb89051ca435d232
Coline Chataing <coline.chataing@telecomnancy.eu>
2021-12-01 13:31:14 +0100

    struct param

--- src/ast/StructParam.java
+++ src/ast/StructParam.java
@@ -1,18 +1,20 @@
 package ast;
 
-public class IntParam implements Ast{
+public class StructParam implements Ast{
 
     public <T> T accept(AstVisitor<T> visitor){
         return visitor.visit(this);
     }
 
-    public Ident ident ;
+    public Ident ident1 ;
+    public Ident ident2 ;
     public Ast listParam;
     public Ast bloc;
 
-    public IntParam(Ident ident, Ast listParam, Ast bloc){
-        this.ident = ident;
+    public StructParam(Ident ident1, Ident ident2, Ast listParam, Ast bloc){
+        this.ident1 = ident1;
+        this.ident2 = ident2; 
         this.listParam = listParam;
-        this.bloc=bloc;
+        this.bloc = bloc;
     }
 }
