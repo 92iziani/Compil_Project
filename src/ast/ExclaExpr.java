5c9d6bc3b9a4da66a4b6b8d445925e7ea4efbb8d
ZIANI Ahmed <ahmed.ziani@telecomnancy.eu>
2021-12-01 13:19:56 +0000

    Upload New File

---
+++ src/ast/ExclaExpr.java
@@ -0,0 +1,14 @@
+package ast;
+
+public class ExclaExpr implements Ast {
+
+    public <T> T accept(AstVisitor<T> visitor){
+        return visitor.visit(this);
+    }
+
+    public Ast expr;
+
+    public ExclaExpr(Ast expr){
+        this.expr = expr;
+    }
+}
