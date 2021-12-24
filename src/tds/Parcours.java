79ea8a44f8458d85a9e8620c48735d0c5da2b3d4
Laury Thiebaux <laury.thiebaux@telecomnancy.eu>
2021-12-24 16:04:16 +0100

    init pour la tds

---
+++ src/tds/Parcours.java
@@ -0,0 +1,245 @@
+package tds;
+
+import ast.Affectation;
+import ast.AstVisitor;
+import ast.Bloc;
+import ast.DeclTyp;
+import ast.DeclaAffect;
+import ast.DeclaList;
+import ast.Divide;
+import ast.Entier;
+import ast.ExclaExpr;
+import ast.Fleche;
+import ast.Ident;
+import ast.IdentExprPointeur;
+import ast.IfThen;
+import ast.IfThenElse;
+import ast.IntParam;
+import ast.List;
+import ast.ListeDeclVars;
+import ast.ListeExpr;
+import ast.ListeInstruction;
+import ast.Minus;
+import ast.Mult;
+import ast.OpExpr;
+import ast.Operateur;
+import ast.Paramint;
+import ast.Paramstruct;
+import ast.ParenthExpr;
+import ast.Plus;
+import ast.Program;
+import ast.Return;
+import ast.Sizeof;
+import ast.Struct;
+import ast.StructParam;
+import ast.TiretExpr;
+import ast.Vide;
+import ast.While;
+
+public class Parcours implements AstVisitor {
+
+    @Override
+    public Object visit(Program program) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(IfThen ifthen) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(IfThenElse ifthenelse) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(While whil) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Return retur) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Affectation affect) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Ident ident) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(ListeDeclVars liste) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(ListeInstruction liste) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Bloc bloc) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Plus plus) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Minus minus) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Divide divide) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Mult mult) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(DeclaList declaList) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(DeclTyp declTyp) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(DeclaAffect dAffect) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Entier entier) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(IntParam intParam) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Struct struct) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(StructParam structParam) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(ExclaExpr x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Fleche x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(IdentExprPointeur x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Operateur x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(OpExpr x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(ParenthExpr x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Sizeof x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(TiretExpr x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(ListeExpr listexpr) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Paramint x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Paramstruct x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(List x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object visit(Vide x) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ src/tds/Tds.java
@@ -0,0 +1,17 @@
+package tds;
+
+import java.util.ArrayList;
+
+public class Tds {
+    
+    private int num_region;
+    private int num_imbrication;
+    private int num_table_mere;
+    private ArrayList contenu;
+
+    public Tds(){
+
+    }
+    
+
+}
