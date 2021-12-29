3cc98377eac206609affa53b2d35b8892cae1a32
Laury Thiebaux <laury.thiebaux@telecomnancy.eu>
2021-12-29 20:08:21 +0100

    commentaires pour la compréhension de ligneé

--- src/tds/Ligne.java
+++ src/tds/Ligne.java
@@ -1,5 +1,8 @@
 package tds;
 
+//représente une ligne de la tds par exemple une variable
+// chaque type de ligne extends cette classe car chaque ligne n'a pas les mêmes informations
+
 public abstract class Ligne {
     
 }
