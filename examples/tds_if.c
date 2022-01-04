957641396e72db762c609f5dd04b0aba7e55c6ce
Laury Thiebaux <laury.thiebaux@telecomnancy.eu>
2022-01-04 14:10:57 +0100

    example plus complexe

--- examples/tds_if.c
+++ examples/tds_if.c
@@ -1,9 +1,12 @@
 int main(){
     int b=3;
+    int result;
     if (b==3){
         b=5;
+        int c=1;
+        result = b+c;
     }
-
+    return result;
 }
 int fork(){
 int a;
@@ -12,5 +15,7 @@ if (a==4){
     a=0;
 }else {
     a=1;
+    int test=25;
 }
+return a;
 }
