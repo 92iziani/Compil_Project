566a5ac71c10acf124f638dbf1c4a72d89659038
Coline Chataing <coline.chataing@telecomnancy.eu>
2022-01-04 10:42:09 +0100

    examples

--- examples/correct.c
+++ examples/correct.c
@@ -1,14 +1,11 @@
-int a = 2;
-int b = 0;
-int c = 15;
-int x;
-int x,y;
-int x,y,z;
-
+int a;
+int b = 5;
+int c;
 
 /* test de commentaire */
 
-//gros test
+//test de commentaire
+
 int basique(){
     return x;
 }
@@ -18,7 +15,7 @@ int d;
 int f=0;
 if (a) {
     b=1+2*3+4;
-} 
+}
 else {
     b=2;
 }
---
+++ examples/false.c
@@ -0,0 +1,12 @@
+int a;
+int b = 5;
+int c;
+
+
+int funk(){
+    return o;
+}
+int main{
+b=2*b;
+return b;}
+// o n'a pas été défini
